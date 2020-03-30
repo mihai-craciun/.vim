19b2e2a84623b148e2c698de4325305007d77462
Mihai Craciun <mihaicraciun96@gmail.com>
2020-03-30 21:38:58 +0300

    Updated script
    Signed-off-by: Mihai Craciun <mihaicraciun96@gmail.com>

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 # Get the parent directory of the .vim folder
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 # Copy to home
-cp -Tr "$DIR/.vim" "$HOME/"
+cp -r "$DIR/.vim" "$HOME/"
 # Link the .vimrc
-cp -T "$HOME/.vim/.vimrc" "$HOME/"
+cp "$HOME/.vim/.vimrc" "$HOME/"
 # Checkout Vundle to latest
 CRT_DIR=$(pwd)
 cd "$HOME/.vim/bundle/Vundle.vim"
 git pull origin master
-git checkout origin/master
+git checkout master
 # Update commands
 vim +PluginUpdate +qall
 # Change back
